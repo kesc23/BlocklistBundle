bb4c51c10d4284ae96ee0d5363390365127517a0
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-08 17:45:34 -0300

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,14 @@ return array(
     'description' => 'Este plugin cria uma blacklist de emails os quais não terão dados mantidos nem receberao conteúdo das campanhas.',
     'author'      => 'Kesc23 (Kevin Campos)',
     'version'     => '1.0.0',
+    'routes'      => array(
+        'main'    => array(
+            'plugin_blocklist_main' => array(
+                'path'       => '/blocklist',
+                'controller' => 'BlocklistBundle:Default:main'
+            )
+        )
+    ),
     'menu'        => array(
         'main' => array(
             'priority' => 4,
