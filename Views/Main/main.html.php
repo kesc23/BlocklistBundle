8ef60326dd02d9dcbc6339d002a2d1ed3357245d
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-14 15:35:50 -0300

    Update main.html.php

--- Views/Main/main.html.php
+++ Views/Main/main.html.php
@@ -1,4 +1,5 @@
 <?php
+global $blocklist_postCond;
 $view->extend( 'MauticCoreBundle:Default:content.html.php' );
 
 $header = 'Block List';
@@ -6,16 +7,25 @@ $header = 'Block List';
 ?>
 <div class="content-body">
     <div class="pa-md">
-        <?php if( ! $end ): ?>
+        <?php if( ! $blocklist_postCond ): ?>
         <div style="margin: 0px auto">
             <form target="_self" method="post" action="/s/blocklist">
                 <fieldset>
-                    <label class="control-label required" for="clientKey">Seu client Id</label>
-                    <input class="form-control" type="text" name="clientKey" id="clientKey">
+                    <label class="control-label required" for="Blocklist_mauticBaseUrl">Sua URL Base</label>
+                    <input
+                        class="form-control"
+                        type="text"
+                        value="<?php echo "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['REMOTE_ADDRESS']}" ?>"
+                        name="Blocklist_mauticBaseUrl"
+                        id="Blocklist_mauticBaseUrl">
                 </fieldset>
                 <fieldset>
-                    <label class="control-label required" for="clientSecret">Seu client secret</label>
-                    <input class="form-control" type="password" name="clientSecret" id="clientSecret">
+                    <label class="control-label required" for="clientKey">Seu client Id</label>
+                    <input class="form-control" type="text" name="Blocklist_clientKey" id="Blocklist_clientKey">
+                </fieldset>
+                <fieldset>
+                    <label class="control-label required" for="Blocklist_clientSecret">Seu client secret</label>
+                    <input class="form-control" type="password" name="Blocklist_clientSecret" id="Blocklist_clientSecret">
                 </fieldset>
                 <button type="submit">Enviar</button>
             </form>
