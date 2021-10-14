73b5dd2678efc9e8459af5156647b021ac1a7cd1
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-14 14:26:04 -0300

    Update main.html.php

--- Views/Main/main.html.php
+++ Views/Main/main.html.php
@@ -1,22 +1,24 @@
 <?php
 
 ?>
-<div class="content-body pa-md">
-    <?php if( ! $end ): ?>
-    <div style="margin: 0px auto">
-        <form action="/api/blocklist-bundle/oauth" method="post">
-            <fieldset>
-                <label class="control-label required" for="clientKey">Seu client Id</label>
-                <input class="form-control" type="text" name="clientKey" id="clientKey">
-            </fieldset>
-            <fieldset>
-                <label class="control-label required" for="clientSecret">Seu client secret</label>
-                <input class="form-control" type="password" name="clientSecret" id="clientSecret">
-            </fieldset>
-            <button type="submit">Enviar</button>
-        </form>
-    </div>
-    <?php endif ?>
+<div class="content-body">
+    <div class="pa-md">
+        <?php if( ! $end ): ?>
+        <div style="margin: 0px auto">
+            <form action="/api/blocklist-bundle/oauth" method="post">
+                <fieldset>
+                    <label class="control-label required" for="clientKey">Seu client Id</label>
+                    <input class="form-control" type="text" name="clientKey" id="clientKey">
+                </fieldset>
+                <fieldset>
+                    <label class="control-label required" for="clientSecret">Seu client secret</label>
+                    <input class="form-control" type="password" name="clientSecret" id="clientSecret">
+                </fieldset>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+        <?php endif ?>
 
-    <h1>Hello!</h1>
+        <h1>Hello!</h1>
+    </div>
 </div>
