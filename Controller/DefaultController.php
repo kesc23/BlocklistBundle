b8030ab4937f152d1ab48ae3fcde43b2ba50936f
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-14 15:54:08 -0300

    Update DefaultController.php

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -25,6 +25,60 @@ class DefaultController extends CommonController
 
     public function verifyOAuth()
     {
-        \file_put_contents( __DIR__.'api.log', json_encode( $_POST ) );
+       \file_put_contents( __DIR__.'api.log', json_encode( $_POST ) );
+
+       global $settings;
+
+        /**
+         * @todo load this array from database or config file
+         */
+        $accessTokenData = array(
+            'accessToken' => '',
+            'accessTokenSecret' => '',
+            'accessTokenExpires' => ''
+        );
+
+        /**
+         * @todo Sanitize this URL. Make sure it starts with http/https and doesn't end with '/'
+         */
+        $mauticBaseUrl = urlencode( rtrim( $_POST['Blocklist_mauticBaseUrl'] ) );
+
+        /**
+         * @todo Change this to your app callback. It should be the same as you entered when you were creating your Mautic API credentials.
+         */
+        $settings = array(
+            'baseUrl'           => $mauticBaseUrl,
+            'clientKey'         => $_POST['Blocklist_clientKey'],
+            'clientSecret'      => $_POST['Blocklist_clientSecret'],
+            'callback'          => rtrim( $mauticBaseUrl, "/s" ) . '/s/blocklist/authenticated',
+            'version'           => 'OAuth2'
+        );
+
+        if( ! empty( $accessTokenData['accessToken'] ) && ! empty( $accessTokenData['accessTokenSecret'] ) )
+        {
+            $settings['accessToken']        = $accessTokenData['accessToken'] ;
+            $settings['accessTokenSecret']  = $accessTokenData['accessTokenSecret'];
+            $settings['accessTokenExpires'] = $accessTokenData['accessTokenExpires'];
+        }
+
+        $auth = \Mautic\Auth\ApiAuth::initiate( $settings );
+
+        if( $auth->validateAccessToken() ) 
+        {
+            if( $auth->accessTokenUpdated() )
+            {
+                $accessTokenData = $auth->getAccessTokenData();
+                /** @todo Save $accessTokenData */
+                /** @todo Display success authorization message */
+            } else {
+                /** @todo Display info message that this app is already authorized. */
+            }
+        } else {
+            /** @todo Display info message that the token is not valid. */
+        }
+
+        return $this->delegateView(
+            array( 'contentTemplate' => 'BlocklistBundle:Main:main.html.php' )
+        );
     }
 }
