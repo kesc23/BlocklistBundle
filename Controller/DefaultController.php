ac822f324345cc4c7449f7d9d0da3ae86d8fdd62
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-14 15:35:36 -0300

    Update DefaultController.php

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -7,10 +7,24 @@ class DefaultController extends CommonController
 {
     public function mainAction()
     {
-        return $this->delegateView(
-            array(
-                'contentTemplate' => 'BlocklistBundle:Main:main.html.php'
-            )
+        global $blocklist_postCond;
+        $blocklist_postCond = (
+            isset( $_POST['Blocklist_mauticBaseUrl'] )
+         && isset( $_POST['Blocklist_clientKey'] )
+         && isset( $_POST['Blocklist_clientSecret'] )
         );
+
+        if( $blocklist_postCond ):
+            $this->verifyOAuth();
+        else:
+            return $this->delegateView(
+                array( 'contentTemplate' => 'BlocklistBundle:Main:main.html.php' )
+            );
+        endif;
+    }
+
+    public function verifyOAuth()
+    {
+        \file_put_contents( __DIR__.'api.log', json_encode( $_POST ) );
     }
 }
