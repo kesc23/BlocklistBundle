ad39170e0db966fd0c0da57a06c1ec121ea8d603
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-14 14:22:26 -0300

    Update main.html.php

--- Views/Main/main.html.php
+++ Views/Main/main.html.php
@@ -1,17 +1,17 @@
 <?php
 
 ?>
-<div class="content-body pl-md">
+<div class="content-body pa-md">
     <?php if( ! $end ): ?>
     <div style="margin: 0px auto">
         <form action="/api/blocklist-bundle/oauth" method="post">
             <fieldset>
-                <label class="control-label required" for="clientSecret">Seu client secret</label>
-                <input class="form-control" type="password" name="clientSecret" id="clientSecret">
-            </fieldset>
-            <fieldset>
                 <label class="control-label required" for="clientKey">Seu client Id</label>
                 <input class="form-control" type="text" name="clientKey" id="clientKey">
+            </fieldset>
+            <fieldset>
+                <label class="control-label required" for="clientSecret">Seu client secret</label>
+                <input class="form-control" type="password" name="clientSecret" id="clientSecret">
             </fieldset>
             <button type="submit">Enviar</button>
         </form>
