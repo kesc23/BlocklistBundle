d86307d332cca0faa719f7762bf8eef1059c6d97
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-20 15:39:14 -0300

    Update main.html.php

--- Views/Main/main.html.php
+++ Views/Main/main.html.php
@@ -6,12 +6,12 @@ $header = 'Block List';
 ?>
 <div class="content-body">
     <div class="pa-md">
-        <pre><?php
-        print_r( $contact->addToBlocklist( 'kesc23@hotmail.com' ) );
-        print_r( $contact->getTables() );
-        print_r( $contact->dostos() );
-        ?></pre>
-        <h1>blocklist title!</h1>
+        <!-- <pre><?php  
+        # print_r( $contact->addToBlocklist( 'kesc23@hotmail.com' ) );
+        # print_r( $contact->getTables() );
+        # print_r( $contact->dostos() );
+        ?></pre>-->
+        <h1><?php echo $view['translator']->trans('plugin.helloworld.titleh1') ?></h1>
         <div class="dashboard-widgets cards">
             <div class="card-flex widget" style="width: 50%">
                 <div class="card" style="padding: 5px"></div>
@@ -20,7 +20,7 @@ $header = 'Block List';
                 <div class="card" style="padding: 5px">
                     <?php $leads = $contact->getLeadEmails();
                     if( $leads ):?>
-                    <h3>Esses contatos serão deletados</h3>
+                    <h3><?php echo $view['translator']->trans('plugin.helloworld.deletemsg') ?></h3>
                     <div style="max-height: 400px; padding: 5px; box-shadow: inset 0px 0px 4px rgb(0 0 0 / 15%);border-radius: 3px; overflow-y: auto;"><?php
                         foreach( $leads as $lead )
                         {
