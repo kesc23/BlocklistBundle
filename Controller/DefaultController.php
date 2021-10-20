b4101685894b4cd37a68665b5fe22c1854746524
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-20 10:01:09 -0300

    Update DefaultController.php

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -16,4 +16,34 @@ class DefaultController extends CommonController
             )
         );
     }
+
+    public function deleteAction()
+    {
+        $contact = $this->getModel( 'blocklist.contact' );
+        $blocked = $contact->getFromBlocklist();
+        $leads   = $contact->getLeads();
+        $tables  = array();
+        $ids     = array();
+
+
+        foreach( $contact->getTables() as $table )
+        {
+            $tables[] = $table['TABLE_NAME'];
+        }
+
+        foreach( $leads as $lead )
+        {
+            if( in_array( $lead['email'], $blocked ) )
+            {
+                $ids[] = (int) $lead['id'];
+            }
+        }
+
+        print_r( $ids ); print_r( $table );
+
+        foreach( $tables as $table )
+        {
+            # $contact->deleteLeads( $ids, $table );
+        }
+    }
 }
