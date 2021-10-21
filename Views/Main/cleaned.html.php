d4adebba718f848bc2563d337f30c5b96b6b59f9
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-21 17:49:34 -0300

    tradução br adicionada

--- Views/Main/cleaned.html.php
+++ Views/Main/cleaned.html.php
@@ -4,13 +4,21 @@ $view->extend( 'MauticCoreBundle:Default:content.html.php' );
 <div class="content-body">
     <div class="pa-md">
         <h1><?php echo $view['translator']->trans('plugin.blocklist.cleaned') ?></h1>
-        <?php
+        <div class="dashboard-widgets cards">
+            <div class="card" style="width: 100%; padding: 5px">    
+            <?php
             $emails = $contact->getOnlyDeletedLeads();
             empty( $emails ) ? $emails = [] : $emails;
             foreach( $emails as $lead )
             {
                 echo "<p>Email: {$lead}</p>";
             }
-        ?>
+         ?></div>
+        </div>
     </div>
 </div>
+
+<div class="dashboard-widgets cards">
+    <div class="card" style="width: 100%; padding: 5px">
+
+    
