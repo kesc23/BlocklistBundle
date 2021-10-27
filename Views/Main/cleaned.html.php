db922a110e6355271139b9fac7077ec2275a90be
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-27 10:43:55 -0300

    Update cleaned.html.php

--- Views/Main/cleaned.html.php
+++ Views/Main/cleaned.html.php
@@ -1,22 +1,33 @@
 <?php
+/**
+ * @package Blocklist
+ * @author  Kevin Campos
+ * @version 1.1.0
+ * @license GPL v3 or later
+ */
 $view->extend( 'MauticCoreBundle:Default:content.html.php' );
 ?>
 <div class="content-body">
     <div class="pa-md">
         <h1><?php echo $view['translator']->trans('plugin.blocklist.cleaned') ?></h1>
         <div class="dashboard-widgets cards">
-            <div class="card" style="width: 100%; padding: 5px">    
-                <?php
-                $emails = $contact->getOnlyDeletedLeads();
-                empty( $emails ) ? $emails = [] : $emails;
+        <?php
+            $emails = $contact->getOnlyDeletedLeads();
+            if( ! empty( $emails ) ):
+            ?><div class="card" style="width: 100%; padding: 5px">    
+            <?php                
                 foreach( $emails as $lead )
                 {
                     echo "<p>Email: {$lead}</p>";
                 }
                 ?>
+            </div><?php
+            else:?>
             </div>
-        </div>
+            <div style="margin: 70px auto; opacity: .5; text-align: center">
+                <h2><?php echo $view['translator']->trans('plugin.blocklist.nodeleted') ?></h2>
+            <?php
+            endif;
+        ?></div>
     </div>
-</div>
-
-    
+</div>    
