7319b88638e65bbfac310203819d444ef674ed68
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-10-27 10:40:53 -0300

    added leadsubscriber event

--- BlocklistBundle.php
+++ BlocklistBundle.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @package Blocklist
+ * @author  Kevin Campos
+ * @version 1.1.0
+ * @license GPL v3 or later
+ */
 namespace MauticPlugin\BlocklistBundle;
 
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
