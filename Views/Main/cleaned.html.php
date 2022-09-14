86efed5333f4efae760b00de0d0957a600de5eee
Kevin Campos <kesc23@hotmail.com>
2022-09-14 01:16:03 -0300

    Update cleaned.html.php
    * added proper header title
    * preparing a way to make pagination viable

--- Views/Main/cleaned.html.php
+++ Views/Main/cleaned.html.php
@@ -2,26 +2,36 @@
 /**
  * @package Blocklist
  * @author  Kevin Campos
- * @version 1.1.0
  * @license GPL v3 or later
  */
+
 $view->extend( 'MauticCoreBundle:Default:content.html.php' );
+$tr = $view['translator'];
+$view['slots']->set( 'headerTitle', $tr->trans( 'plugin.blocklist.cleaned' ) );
+
+$page = isset( $_GET['page'] ) ? filter_var( $_GET[ 'page' ], FILTER_VALIDATE_INT ) : 1;
+
+$start = ( $page -1 ) * 20;
+$end   = $page * 20;
+
+$count = (int) ceil(sizeof( $emails ) / 20);
+
 ?>
 <div class="content-body">
     <div class="pa-md">
-        <h1><?php echo $view['translator']->trans('plugin.blocklist.cleaned') ?></h1>
         <div class="dashboard-widgets cards">
         <?php
-            $emails = $contact->getOnlyDeletedLeads();
             if( ! empty( $emails ) ):
-            ?><div style="margin: 70px auto; opacity: .5; text-align: center">
+            ?><div style="margin: auto; opacity: .5; text-align: center">
                 <h3><?php echo $view['translator']->trans('plugin.blocklist.totaldeleted') . ": " . count( $emails ) ?></h3>
+                <h5><?php echo "showing from $start to $end" ?></h5>
+                <h1><?php echo "$count" ?></h1>
             </div>
-            <div class="card" style="width: 100%; padding: 5px">
+            <div class="panel" style="width: 100%; padding: 5px">
                 <?php                
-                    foreach( $emails as $lead )
+                    for( $i = $start; $i < $end; $i ++ )
                     {
-                        echo "<p>{$lead}</p>";
+                        echo "<p>{$emails[$i]}</p>";
                     }
                 ?>
             </div><?php
@@ -31,6 +41,10 @@ $view->extend( 'MauticCoreBundle:Default:content.html.php' );
                 <h2><?php echo $view['translator']->trans('plugin.blocklist.nodeleted') ?></h2>
             <?php
             endif;
-        ?></div>
+        ?>
+        <div style="display: flex">
+
+        </div>
+        </div>
     </div>
 </div>    
